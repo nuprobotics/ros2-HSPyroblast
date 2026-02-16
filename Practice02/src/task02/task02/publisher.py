ce025a1225acfa124e160f6e83949a919c18f584
HSPyroblast <levsovga@ya.ru>
2026-02-16 21:05:04 +0300

    Solution 1-3

--- Practice02/src/task02/task02/publisher.py
+++ Practice02/src/task02/task02/publisher.py
@@ -1,6 +1,7 @@
 import rclpy
 from rclpy.node import Node
 from std_msgs.msg import String
+import time
 
 class Publisher(Node):
     def __init__(self):
@@ -9,24 +10,26 @@ class Publisher(Node):
         self.declare_parameter('topic_name', '/spgc/receiver')
         self.declare_parameter('text', 'Hello, ROS2!')
 
+        self.publisher = None
+
+    def start(self):
         topic_name = self.get_parameter('topic_name').get_parameter_value().string_value
         self.text = self.get_parameter('text').get_parameter_value().string_value
 
         self.publisher = self.create_publisher(String, topic_name, 10)
-        self.get_logger().info(f"Publisher started on topic '{topic_name}' with default text '{self.text}'")
+        self.get_logger().info(f"Publisher started on topic '{topic_name}' with text '{self.text}'")
 
-        self.publish_message()
-    
-    def publish_message(self):
+        time.sleep(0.05)
+
         msg = String()
         msg.data = self.text
         self.publisher.publish(msg)
         self.get_logger().info(f'Published message: "{msg.data}"')
-        
 
 def main(args=None):
     rclpy.init(args=args)
     node = Publisher()
-    rclpy.spin_once(node)
+    rclpy.spin_once(node, timeout_sec=0.1)
+    node.start()
     node.destroy_node()
     rclpy.shutdown()
