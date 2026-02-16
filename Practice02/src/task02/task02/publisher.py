7eccd9e59809ce360dc8d85bfc1b02950e9642c2
HSPyroblast <levsovga@ya.ru>
2026-02-16 21:49:52 +0300

    Solution 1-3

--- Practice02/src/task02/task02/publisher.py
+++ Practice02/src/task02/task02/publisher.py
@@ -13,10 +13,18 @@ class Publisher(Node):
         self.text = self.get_parameter('text').get_parameter_value().string_value
 
         self.publisher = self.create_publisher(String, topic_name, 10)
+        self.get_logger().info(f"Publisher started on topic '{topic_name}' with default text '{self.text}'") 
+        self.add_on_set_parameters_callback(self.parameters_callback)   
         self.timer = self.create_timer(1.0, self.publish_message)
-        self.get_logger().info(f"Publisher started on topic '{topic_name}' with default text '{self.text}'")
 
-        self.publish_message()
+    def parameters_callback(self, params):
+        for param in params:
+            if param.name == 'text':
+                self.text = param.value
+                self.get_logger().info(f'Parameter text updated to: "{self.text}"')
+                if self.publish_timer:
+                    self.publish_and_exit()
+        return rclpy.node.SetParametersResult(successful=True)
     
     def publish_message(self):
         msg = String()
