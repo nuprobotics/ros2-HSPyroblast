e8ba6da127fc670e8b577be2e6b007e10e313c32
HSPyroblast <levsovga@ya.ru>
2026-02-16 20:23:41 +0300

    Solution 1-3

--- Practice02/src/task01/setup.py
+++ Practice02/src/task01/setup.py
@@ -9,7 +9,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/task01.launch.py']),
+        ('share/' + package_name + '/launch', ['launch/task01.launch']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
